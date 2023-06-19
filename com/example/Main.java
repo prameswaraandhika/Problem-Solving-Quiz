2fe9d7bd3ea4d3c2d368fa95e5bcf0831dc13067
Andhikap <prameswaara@gmail.com>
2023-06-19 09:35:44 +0700

    Given an ArrayList of strings, remove all strings that start with the letter A done

---
+++ com/example/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+ * Problem: Given an ArrayList of strings, remove all strings that start with the letter "A"
+ */
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList("Anggur", "alpukat", "Apel", "Pisang", "Coklat", "Melon"));
+
+        Iterator<String> iter = list.iterator();
+        while (iter.hasNext()) {
+            String removeValue = iter.next().toUpperCase();
+            if (removeValue.startsWith("A")) {
+                System.out.println("Removig " + removeValue);
+                iter.remove();
+            }
+        }
+
+        System.out.println(list.toString());
+    }
+}
